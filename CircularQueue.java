75bf8bd8bd72d9bf66d903ac4386474eb8a2a80e
evan999 <emeshberge@gmail.com>
2020-09-10 23:17:49 -0400

    Circular Queue implementation. Passes initial test case. Needs to be debugged.

---
+++ CircularQueue.java
@@ -0,0 +1,74 @@
+class MyCircularQueue {
+    
+	private int[] queue;
+	private int head;
+	private int tail;
+	private int size;
+
+	public MyCircularQueue(int k) {
+			queue = new int[k];
+			size = k;
+			head = -1;
+			tail = -1;
+	}
+	
+
+	public boolean enQueue(int value) {
+
+			if(isFull()==true){
+					return false;
+			}
+			
+			else if(isEmpty()==true){
+					head = 0;
+			}
+			tail = (tail + 1) % size;
+			queue[tail] = value;
+			return true;
+	}
+
+	public boolean deQueue() {
+			if(isEmpty()==true){
+					return false;
+			}
+			
+			if (head==tail){
+					head = -1;
+					tail = -1;
+			}
+			
+			head = (head + 1) % size;
+			return true;
+	}
+
+	public int Front() {
+			if(isEmpty()==true){
+					return -1;
+			}
+			return queue[head];
+	}
+	
+
+	public int Rear() {
+			if(isEmpty()==true){
+					return -1;
+			}
+			return queue[tail];
+	}
+	
+	public boolean isEmpty() {
+			if(head == -1){
+					return true;
+			}
+			
+			return false;
+	}
+	
+	public boolean isFull() {
+			if((tail + 1) % size == head){
+					return true;
+			}
+			
+			return false;
+	}
+}
