8cd8abb2e0cf7ac3da83b563c0777a10c8a3872a
evan999 <emeshberge@gmail.com>
2020-09-23 22:49:39 -0400

    Ice Cream Parlor - a Two Sum type problem using a hashmap and searches for the pair that adds up to the target value.

---
+++ iceCreamParlorHashMap.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static void whatFlavors(int[] cost, int money) {
+
+        HashMap<Integer, Integer> flavors = new HashMap<>();
+
+        for(int i = 0; i < cost.length; i++){
+
+            int complement = money - cost[i];
+            
+            if(flavors.containsKey(complement)){
+
+                int curIndex = i;
+                int compIndex = flavors.get(complement);
+
+                System.out.println((compIndex + 1) + " " + (i + 1));
+            }
+            else{
+                flavors.put(cost[i], i);
+            }
+        }
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int money = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] cost = new int[n];
+
+            String[] costItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int costItem = Integer.parseInt(costItems[i]);
+                cost[i] = costItem;
+            }
+
+            whatFlavors(cost, money);
+        }
+
+        scanner.close();
+    }
+}
