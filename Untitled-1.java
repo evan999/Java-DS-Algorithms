04d3fd933c64475ee5e79fdd5c36cecab34b1a8e
evan999 <emeshberge@gmail.com>
2021-03-04 23:47:31 -0500

    Attempt at GetOceanViewProperties problem. Algorithm that outputs ocean view properties based on height of each building and distance from ocean.

---
+++ Untitled-1.java
@@ -0,0 +1,18 @@
+static List<Integer> getOceanView(List<Integer> bldheights) {
+        // In input array
+
+        List<Integer> oceanViewApts = new ArrayList<>();
+
+        for(int apt = 0; apt < bldheights.size(); apt++) {
+            for(int nextApt = apt + 1; nextApt < bldheights.size(); nextApt++) {
+                if(bldheights.get(nextApt) < bldheights.get(apt)) {
+                    oceanViewApts.add(bldheights.get(nextApt));
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+
+        return oceanViewApts;
+    }
