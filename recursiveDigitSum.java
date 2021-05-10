495c418b2443c8b3c175dcce89fdcd265d61e3dd
evan999 <emeshberge@gmail.com>
2021-05-09 20:18:45 -0400

    Recursive Digit Sum problem attempt. Did not pass.

---
+++ recursiveDigitSum.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+	/*
+	 * Complete the 'superDigit' function below.
+	 *
+	 * The function is expected to return an INTEGER. The function accepts following
+	 * parameters: 1. STRING n 2. INTEGER k
+	 */
+
+	public static int superDigit(String n, int k) {
+		// Write your code here
+
+		// String newString = " ";
+		String newStr = n;
+
+		if (n.length() == 1) {
+			return Integer.parseInt(n);
+		}
+
+		for (int concat = 0; concat < k; concat++) {
+			newStr = newStr.concat(n);
+			// System.out.println(newStr);
+		}
+
+		// System.out.println(n);
+
+		int len = newStr.length();
+		String curStr = newStr;
+		// System.out.println(curStr);
+		char[] digits = curStr.toCharArray();
+		Integer sum = 0;
+		while (len > 1) {
+			for (int digit = 0; digit < len; digit++) {
+				int value = Character.getNumericValue(digits[digit]);
+				sum += value;
+			}
+			curStr = Integer.toString(sum);
+			len = curStr.length();
+		}
+
+		int superDigit = Integer.parseInt(curStr);
+
+		return superDigit;
+
+		// return 0;
+
+	}
+
+}
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+		String n = firstMultipleInput[0];
+
+		int k = Integer.parseInt(firstMultipleInput[1]);
+
+		int result = Result.superDigit(n, k);
+
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
+}
