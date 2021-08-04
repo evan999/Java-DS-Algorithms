c0b7b43b8adda748724726976325974dbf818c13
evan999 <emeshberge@gmail.com>
2021-08-03 21:56:29 -0400

    Tree with Count of Internal Nodes solution. Passes all tests.

---
+++ InternalNodes.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class InternalNodes {
+
+	public static int count(int[] tree) {
+		int internalNodes = 0;
+		Set<Integer> uniqueNodes = new HashSet<>();
+
+		for (int i = 0; i < tree.length; i++) {
+			if (tree[i] != -1) {
+				uniqueNodes.add(tree[i]);
+			}
+		}
+
+		return uniqueNodes.size();
+	}
+
+	public static void main(String args[]) {
+		System.out.println(InternalNodes.count(new int[] { 1, 3, 1, -1, 3 }));
+	}
+}
